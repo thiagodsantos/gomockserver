3059b1724b140c0295bff2de06491e1886b8775a
Thiago Santos <tadossantos@rd.com.br>
2024-03-25 12:29:21 -0300

    chore: update mock response func

--- storage/mock.go
+++ storage/mock.go
@@ -16,26 +16,32 @@ func GetMockResponse(url string) ([]byte, int, error) {
 	// Check if file exists in current directory
 	fileExists := utils.FileExists(responseFilename)
 
-	if fileExists {
-		var responseFileInfo structs.Response
-		// Read JSON file data from response_<url>.json
-		responseFile, err := utils.ReadJSONFile(responseFilename, &responseFileInfo)
-		if err != nil {
-			fmt.Println("Error reading mock:", err)
-			return nil, 500, err
-		}
-
-		// Return mock response from file
-		if len(responseFile) > 0 {
-			responseFileJSON, err := json.MarshalIndent(responseFileInfo.Body, "", "  ")
-			if err != nil {
-				fmt.Println("Error encoding mock response to JSON:", err)
-				return nil, 500, err
-			}
-
-			return responseFileJSON, responseFileInfo.StatusCode, nil
-		}
+	if !fileExists {
+		return defaultResponse()
 	}
 
+	var responseFileInfo structs.Response
+	// Read JSON file data from response_<url>.json
+	responseFile, err := utils.ReadJSONFile(responseFilename, &responseFileInfo)
+	if err != nil {
+		fmt.Println("Error reading mock:", err)
+		return nil, 500, err
+	}
+
+	if len(responseFile) == 0 {
+		return defaultResponse()
+	}
+
+	// Return mock response from file
+	responseFileJSON, err := json.MarshalIndent(responseFileInfo.Body, "", "  ")
+	if err != nil {
+		fmt.Println("Error encoding mock response to JSON:", err)
+		return nil, 500, err
+	}
+
+	return responseFileJSON, responseFileInfo.StatusCode, nil
+}
+
+func defaultResponse() ([]byte, int, error) {
 	return nil, 200, nil
 }
